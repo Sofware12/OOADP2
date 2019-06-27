6d1c1a87e956a7f91eea413f6bede008d70c29f5
MysticalX <Mystical6196>
2019-06-27 14:26:47 +0800

    .

--- Routes/cctv.js
+++ Routes/cctv.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express();
 const CCTV = require('../models/cctv');
 
-router.get('/sendFootage', (req, res) => {
-    const data = {
-        name: "Camera 1"
+router.post('/sendFootage', (req, res) => {
+    const date = {
+        time: today
     }
-    let {name} = data;
+    let {time} = date;
 
-    CCTV.create([
-        name
+    cameras.create([
+        time
     ])
     .then(name => res.redirect('/viewVideos'))
     .catch(err => console.log(err));
