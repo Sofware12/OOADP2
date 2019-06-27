90d42ae31e14ec95c82333a373f70d07f0a3c084
Yusuf <181308R@mymail.nyp.edu.sg>
2019-06-27 06:21:18 +0100

    jknkjn

--- Routes/main.js
+++ Routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const light_logs=require('../models/LightsDate')
 
 router.get('/', (req, res) => {
 	const title = 'Video Jotter';
